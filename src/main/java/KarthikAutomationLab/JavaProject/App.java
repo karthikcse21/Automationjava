e4274110023d4f38e78b2990a6e4b8b46a9e22b1
Karthik Muruganandham <karthikcse21@gmail.com>
2023-12-27 00:46:14 +0530

    Test1

---
+++ src/main/java/KarthikAutomationLab/JavaProject/App.java
@@ -0,0 +1,13 @@
+package KarthikAutomationLab.JavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( javaConcepts[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/KarthikAutomationLab/JavaProject/javaConcepts.java
@@ -0,0 +1,18 @@
+package KarthikAutomationLab.JavaProject;
+
+public class javaConcepts {
+
+	public void Stringcreation()
+	{
+//	 javaConcepts obj = new javaConcepts();
+	  String obj = "KarthikStringTest";
+	  
+	  System.out.println("String obj is " +obj);
+	}
+	public static void main(String args[])
+	{
+		javaConcepts func =new javaConcepts();
+		
+		func.Stringcreation();
+	}
+}
